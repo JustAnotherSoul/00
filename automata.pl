cdbcc48ea2097ac41a39ae12b33b61b6828d6ebb
cbasile <cbasile@zixcorp.com>
2017-02-24 16:27:55 -0600

    Fixing ends to be circular instead of assuming zeroes at either end

--- automata.pl
+++ automata.pl
@@ -5,19 +5,23 @@
 % WIth the bracketed numbers indicating that there is an assumption of 0's at either end
 
 %Takes an initial list, control list, and output list and fills in output list
-computeNext([0,0,0 | T], Control, [X | T2]) :- get(0, Control, X), computeNext([0,0 | T], Control, T2).
-computeNext([0,0,1 | T], Control, [X | T2]) :- get(1, Control, X), computeNext([0,1 | T], Control, T2). 
-computeNext([0,1,0 | T], Control, [X | T2]) :- get(2, Control, X), computeNext([1,0 | T], Control, T2). 
-computeNext([0,1,1 | T], Control, [X | T2]) :- get(3, Control, X), computeNext([1,1 | T], Control, T2). 
-computeNext([1,0,0 | T], Control, [X | T2]) :- get(4, Control, X), computeNext([0,0 | T], Control, T2). 
-computeNext([1,0,1 | T], Control, [X | T2]) :- get(5, Control, X), computeNext([0,1 | T], Control, T2). 
-computeNext([1,1,0 | T], Control, [X | T2]) :- get(6, Control, X), computeNext([1,0 | T], Control, T2). 
-computeNext([1,1,1 | T], Control, [X | T2]) :- get(7, Control, X), computeNext([1,1 | T], Control, T2). 
+computeNext([0,0,0 | T], Control, [X | T2], First) :- get(0, Control, X), computeNext([0,0 | T], Control, T2, First).
+computeNext([0,0,1 | T], Control, [X | T2], First) :- get(1, Control, X), computeNext([0,1 | T], Control, T2, First). 
+computeNext([0,1,0 | T], Control, [X | T2], First) :- get(2, Control, X), computeNext([1,0 | T], Control, T2, First). 
+computeNext([0,1,1 | T], Control, [X | T2], First) :- get(3, Control, X), computeNext([1,1 | T], Control, T2, First). 
+computeNext([1,0,0 | T], Control, [X | T2], First) :- get(4, Control, X), computeNext([0,0 | T], Control, T2, First). 
+computeNext([1,0,1 | T], Control, [X | T2], First) :- get(5, Control, X), computeNext([0,1 | T], Control, T2, First). 
+computeNext([1,1,0 | T], Control, [X | T2], First) :- get(6, Control, X), computeNext([1,0 | T], Control, T2, First). 
+computeNext([1,1,1 | T], Control, [X | T2], First) :- get(7, Control, X), computeNext([1,1 | T], Control, T2, First). 
 
-computeNext([0,0], Control, [X]) :- get(0, Control, X).
-computeNext([0,1], Control, [X]) :- get(2, Control, X).
-computeNext([1,0], Control, [X]) :- get(4, Control, X).
-computeNext([1,1], Control, [X]) :- get(6, Control, X).
+computeNext([0,0], Control, [X], 0) :- get(0, Control, X).
+computeNext([0,0], Control, [X], 1) :- get(1, Control, X).
+computeNext([0,1], Control, [X], 0) :- get(2, Control, X).
+computeNext([0,1], Control, [X], 1) :- get(3, Control, X).
+computeNext([1,0], Control, [X], 0) :- get(4, Control, X).
+computeNext([1,0], Control, [X], 1) :- get(5, Control, X).
+computeNext([1,1], Control, [X], 0) :- get(6, Control, X).
+computeNext([1,1], Control, [X], 1) :- get(7, Control, X).
 
 get(N, [H|T], X) :- N>0, I is N-1, get(I, T, X).
 get(0, [H|T], H).
@@ -27,7 +31,16 @@ writeAutomata(0) :-
 writeAutomata(1) :-
     write('#').
 
+get_first([H|T], H).
+
+get_last([H],H).
+get_last([H|T], Last) :- get_last(T, Last).
+
 oneDimensionalAutomata(A,B) :-
     maplist(writeAutomata, A), nl,
-    computeNext([0|A],B,C), 
-    ( A \= C -> oneDimensionalAutomata(C,B) ; true).
+    get_first(A, First),
+    get_last(A, Last),
+    computeNext([Last|A],B,C, First),
+    (A \= C -> oneDimensionalAutomata(C,B) ; true), !.
+ 
+    
